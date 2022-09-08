e013c94ae8a34a652332baa7c3ae0dc234908a7c
kalikho <=>
2022-09-09 01:48:27 +0530

    Added Client Scripts

---
+++ ggmpc-client/client_script.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import pickle,sys,json
+import ecdsa, unittest
+import ggmpc,pickle
+import ggmpc.curves as curves
+import jsonpickle
+
+def client():
+    mpc = ggmpc.Ecdsa(curves.secp256k1)
+    with open('keygen-client', 'rb') as config_dictionary_file:
+        config_dictionary = pickle.load(config_dictionary_file)
+    message = str.encode(sys.argv[1])
+    signed_message = mpc.sign(message, (config_dictionary[0], config_dictionary[1]))
+    with open('client-signed-message.json', 'w+') as fp:
+        json.dump(jsonpickle.encode(signed_message), fp)
+    with open('client-signed-message_pkl', 'wb') as config_dictionary_file:
+        pickle.dump(signed_message, config_dictionary_file)
+client()
---
+++ ggmpc-client/keygen.py
@@ -0,0 +1,31 @@
+import ecdsa, unittest
+import ggmpc,pickle
+import ggmpc.curves as curves
+
+def test_2_in_2_of_3():
+	print("== Generating Client and Server Keys ==")
+	mpc = ggmpc.Ecdsa(curves.secp256k1)
+	A = mpc.key_share(1,2,2)
+	B = mpc.key_share(2,2,2)
+	A, B =\
+		mpc.key_combine((A[1],B[1])),\
+		mpc.key_combine((A[2],B[2])),
+	A = mpc.sign_challenge((A[1], A[2]))
+	BA = mpc.sign_share((A[2], B[2]))
+	AB = mpc.sign_convert((A[1], BA[1]))
+	BA = mpc.sign_convert((BA[2], AB[2]))
+	AB = mpc.sign_convert((AB[1], BA[1]))
+	AB, BA = \
+		mpc.sign_combine((AB,)), \
+		mpc.sign_combine((BA,)),
+
+	server_secret = [AB[1],BA[1]]
+	client_secret = [AB[2],BA[2]]
+	print(type(server_secret))
+	with open('keygen-server', 'wb') as config_dictionary_file:
+		pickle.dump(server_secret, config_dictionary_file)
+	with open('keygen-client', 'wb') as config_dictionary_file:
+		pickle.dump(client_secret, config_dictionary_file)
+
+if __name__ == '__main__':
+	test_2_in_2_of_3()
